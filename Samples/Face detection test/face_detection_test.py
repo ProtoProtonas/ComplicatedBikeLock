297a198cdc881fb18339a392e03b0d947c1eaccd
Augustas Sereika <augis.seras@gmail.com>
2021-02-21 22:23:37 +0200

    Crop face and save in a new .jpg

--- Samples/Face detection test/face_detection_test.py
+++ Samples/Face detection test/face_detection_test.py
@@ -6,7 +6,6 @@ import numpy as np
 import io
 import picamera
 import cv2
-import pickle
 
 print('done importing')
 
@@ -24,6 +23,7 @@ image = cv2.imdecode(buff, 1)
 
 # load cascade for detecting faces
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# the cascade classifier can be downloaded from https://raw.githubusercontent.com/shantnu/Webcam-Face-Detect/master/haarcascade_frontalface_default.xml
 
 # convert to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -38,8 +38,15 @@ faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 print('Found' , len(faces), 'faces')
 
 # draw a rectangle around every found face
-for face in faces:
+for n, face in enumerate(faces):
+	# get the coordinates of a face
 	x, y, w, h = face
+	
+	# crop out each face and save it in a separate .jpg file
+	face_image = image[y:y+h, x:x+w]
+	cv2.imwrite('face%i.jpg' % n, face_image)
+	
+	# draw a rectangle around each face
 	cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 0), 2)
 	
 cv2.imwrite('result.jpg', image)
