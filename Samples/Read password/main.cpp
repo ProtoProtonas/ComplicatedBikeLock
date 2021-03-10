9bfdfac3e15174c680c9ee68687ea7ac307bd6ea
Augustas Sereika <augis.seras@gmail.com>
2021-03-10 16:46:03 +0200

    Read password with no ENTER

--- Samples/Read password/main.cpp
+++ Samples/Read password/main.cpp
@@ -12,11 +12,13 @@ int main()
 
     while (true) {
         cout << "Please enter a character: ";
-        cin >> new_char;
-        // the problem is that after a character is entered ENTER key needs to be pressed
+
+        // use some black linux magic to avoid pressing enter key after every character
+        system("stty raw");
+        new_char = getchar();
+        system("stty cooked");
 
         locker.read_character(new_char);
-
         if (locker.check_password()) { break; }
     }
     return 0;
